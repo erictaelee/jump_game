7e332069cd001e1df05016f46eaa1ea1002dc3a8
Eric <lee.erictae@gmail.com>
2021-06-26 22:34:02 -0500

    add jump function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump() {
-  character.classList.add("animate")
+  if (character.classList != "animate") {
+    character.classList.add("animate");
+  }
+  setTimeout(function () {
+    character.classList.remove("animate")
+  }, 500);
 }
+
+var checkDead = setInterval(function () {
+  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("Game Over");
+  }
+}, 10);
